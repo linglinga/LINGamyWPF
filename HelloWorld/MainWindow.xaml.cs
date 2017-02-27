2f58db788071e6579e6322addb591fa305c73e29
linga <linga@uw.edu>
2017-02-27 14:50:41 -0800

    assignment 1 2/27/17 HelloWorldWPF

---
+++ HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void uxPassword_TextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void uxName_TextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+        private void UxSubmit_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Submitting password:" + uxPassword.Text);
+
+        }
+
+
+    }
+}
